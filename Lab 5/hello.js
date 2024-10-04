9dc0d41ca7fd9fdb5616a320e2cb6dc20af910e9
Thaole <qx6771tj@go.minneapolis.edu>
2024-10-03 20:58:19 -0500

    Lab 5 submission

---
+++ Lab 5/hello.js
@@ -0,0 +1,22 @@
+console.log('Hello World!')
+
+// Three ways to create variables
+let color = 'blue'
+var size = 'medium' // not recommended
+const language = 'Javascript' // can change this value
+// language = 'C#' this is an error
+
+let quantity = 5
+let distance = 4.5
+
+let text = 'Hello World'
+let message = 'Hi Programmers'
+
+console.log('There are ' + quantity + ' programmers. ')
+
+let todaytemp = 75
+console.log("Today\'s temperature is " + todaytemp + 'F')
+
+let tempC = (todaytemp - 32) * 5 / 9
+console.log('Today\s temperature is ' + tempC.toFixed(2) + 'C')
+
